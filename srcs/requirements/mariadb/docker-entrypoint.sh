e2b1739af699a148e989742acaa8ebdd61d63a7d
zakissimo <zakaria.habri@gmail.com>
2023-06-21 01:42:06 +0200

    idk

--- srcs/requirements/mariadb/docker-entrypoint.sh
+++ srcs/requirements/mariadb/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -xe
+set -x
 
 cat << EOF > /etc/my.cnf
 [mysqld]
@@ -12,17 +12,23 @@ skip-networking = false
 bind-address = 0.0.0.0
 EOF
 
+echo $DB_NAME
+echo $DB_USER
+echo $DB_PWD
+
 cp /etc/my.cnf /etc/mysql/my.cnf
 cp /etc/my.cnf ~/my.cnf
 
-/usr/bin/mysql_install_db --user=root --basedir=/usr --datadir=/var/lib/mysql
-/usr/bin/mysqld --user=root --datadir=/var/lib/mysql & sleep 2
+mysql_install_db
+mysqld --user=root --datadir=/var/lib/mysql &
 
-mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
-mysql -e "CREATE USER IF NOT EXISTS $DB_USER\`@'localhost' IDENTIFIED BY '$DB_PWD';"
-mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO $DB_USER@'%' IDENTIFIED BY '$DB_PWD';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_PWD';"
-mysql -e "FLUSH PRIVILEGES;"
+sleep 5
+
+mysql -u root <<EOF
+CREATE DATABASE IF NOT EXISTS $DB_NAME;
+CREATE USER IF NOT EXISTS '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PWD';
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost';
+FLUSH PRIVILEGES;
+EOF
 
 pkill mysqld
-/usr/bin/mysqld --user=root --datadir=/var/lib/mysql
